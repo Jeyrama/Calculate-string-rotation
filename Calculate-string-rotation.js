8285f9ba9a09817f4c69edeb52b751291c954a96
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-06 11:52:19 -0800

    Update Calculate-string-rotation.js

--- Calculate-string-rotation.js
+++ Calculate-string-rotation.js
@@ -1,8 +1,8 @@
 /*
- Write a function that receives two strings and returns n, 
- where n is equal to the number of characters 
- we should shift the first string forward to match the second. 
- The check should be case sensitive.
+Write a function that receives two strings and returns n, 
+where n is equal to the number of characters 
+we should shift the first string forward to match the second. 
+The check should be case sensitive.
 
 For instance, take the strings "fatigue" and "tiguefa". 
 In this case, the first string has been rotated 5 characters 
@@ -27,3 +27,20 @@ function shiftedDiff(first, second) {
 }
 
 // or
+
+function shiftedDiff(first,second){
+  let arr_first = first.split('');
+  let isResult = false;
+  let count_input = 0;
+  for (let index = 0; index < arr_first.length; index++) {
+      let arr_result = arr_first.join('');
+      if (arr_result == second) {
+          isResult = true;
+          break;
+      }
+      let elem = arr_first.pop();
+      arr_first.unshift(elem);
+      count_input++;
+  }
+return (isResult) ? count_input : -1;
+}
